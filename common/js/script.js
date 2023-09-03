a5aa3c163bceff6eaaf210bd15bfd09ba15f1c4c
jahanur96 <jahanurcse96@gmail.com>
2023-09-03 13:39:04 +0600

    added

--- common/js/script.js
+++ common/js/script.js
@@ -31,3 +31,26 @@ valueDisplay.forEach((valueDisplay) => {
         }
     }, timing);
 });
+
+
+
+//search box//
+function showSearchBox() {
+    var searchcontainer = document.getElementById("searchcontainer");
+    searchcontainer.classList.remove("hidden");
+    console.log("hello")
+}
+
+function hideSearchBox() {
+    var searchcontainer = document.getElementById("searchcontainer");
+    searchcontainer.classList.add("hidden");
+    console.log("close")
+}
+
+function performSearch() {
+    var searchInput = document.getElementById("searchInput").value;
+    // Perform your search operation using the search input value
+    // Add your search logic here (e.g., making an API request, filtering data)
+    console.log("Performing search: " + searchInput);
+
+}
